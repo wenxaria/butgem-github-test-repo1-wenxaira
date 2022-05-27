4d59439ba350d89c520356ee4d6892a5e5622750
wenxaria <wenxaria@gmail.com>
2022-05-27 23:14:29 +0300

    wenxaria ekleme yaptı

--- index.aspx.designer.cs
+++ index.aspx.designer.cs
@@ -33,6 +33,15 @@ namespace butgem_github_test_repo1
         protected global::System.Web.UI.WebControls.Label Label1;
 
         /// <summary>
+        /// Label4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+
+        /// <summary>
         /// Label2 control.
         /// </summary>
         /// <remarks>
@@ -76,5 +85,14 @@ namespace butgem_github_test_repo1
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// Button2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
     }
 }
